c09b34c75ceecfadda75e8224919a9aefeec7d92
Pranita <131530404+Pranitha-Gorenka@users.noreply.github.com>
2025-07-07 17:35:31 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "web_sever_as" {
   name                   = "myproject"
-  ami                    =  "ami-000ec6c25978d5999"
+  image_id               = "ami-000ec6c25978d5999"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.bastion_sg.id]
   key_name               = "keypair"
@@ -31,7 +31,7 @@ resource "aws_autoscaling_group" "web_server_asg" {
   desired_capacity        = 2
   health_check_type       = "EC2"
   load_balancers          = [aws_elb.web_server_lb.name]
-  availability_zones       = ["us-east-1a","us-east-1b"]
+  availability_zones      = ["us-east-1a","us-east-1b"]
   launch_template {
        id                 = aws_launch_template.web_server_as.id
        version            = "$Latest"
