3daeb2954112376574157d75ba78f22cae8a1f78
Pranita <131530404+Pranitha-Gorenka@users.noreply.github.com>
2025-07-07 11:52:48 +0200

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,6 +1,6 @@
 variable "ami_id" {
 type = string
-default = "ami-03eb6185d756497f8"
+default = "ami-000ec6c25978d5999"
 }
 
 variable "itype" {
@@ -10,11 +10,11 @@ default = "t2.micro"
 
 variable "kn" {
 type = string
-default = "project-keypair"
+default = "keypair"
 }
 
 variable "iname" {
 type = string
-default = "Swiggy"
+default = "monolithic"
 }
 
