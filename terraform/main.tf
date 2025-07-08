cc6cf33c4d6154ce4126729ad84795e353d13d23
Pranita <131530404+Pranitha-Gorenka@users.noreply.github.com>
2025-07-08 19:18:53 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,7 +54,7 @@ resource "aws_elb" "web_server_lb" {
   subnets         = ["subnet-05ea94e9e07fda83f", "subnet-0f02721f3c50a4541"] # Replace with valid subnet IDs
 
   listener {
-    instance_port     = 8000
+    instance_port     = 8080
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
